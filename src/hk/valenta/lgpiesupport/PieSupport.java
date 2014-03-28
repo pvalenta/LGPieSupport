e0edfdcc0c1b1180657e8de2e7ce641d2411ee31
Pavel Valenta <pvalenta.hk@gmail.com>
2014-03-29 01:32:52 +0800

    Rewritten to set height to 0 instead of disable
    Rewritten to do not disable navigation bar and ring, but instead set
    their height to 0.
    Navigation area then cut 1 pixel so Weather widget and Keyboard should
    not crash.

--- src/hk/valenta/lgpiesupport/PieSupport.java
+++ src/hk/valenta/lgpiesupport/PieSupport.java
@@ -1,65 +1,77 @@
 package hk.valenta.lgpiesupport;
 
-import android.content.res.XResources;
+import android.view.Display;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import de.robv.android.xposed.IXposedHookInitPackageResources;
+import android.view.WindowManager;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_LayoutInflated;
-import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class PieSupport implements IXposedHookInitPackageResources, IXposedHookLoadPackage {
-
-	@Override
-	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
-		// disable navigation bar
-		XResources.setSystemWideReplacement("android", "bool", "config_showNavigationBar", false);
-		
-		if (resparam.packageName.equals("com.android.systemui")) {
-			resparam.res.hookLayout("com.android.systemui", "layout", "global_access_panel", new XC_LayoutInflated() {
-				@Override
-				public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
-					// get it as linear layout
-					LinearLayout panel = (LinearLayout)liparam.view;
-					ViewGroup.LayoutParams params = panel.getLayoutParams();
-					
-					// set 0 height for global access panel
-					XposedBridge.log("global_access_panel height set to 0");
-					params.height = 0;
-				}
-			});
-		}
-	}
+public class PieSupport implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 		// disable navigation ring
 		if (lpparam.packageName.equals("com.android.systemui")) {
 			try {
-				XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBar", lpparam.classLoader, "enableGlobalAccess", boolean.class, new XC_MethodHook() {
+				XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBar", lpparam.classLoader, "getNavigationBarLayoutParams", new XC_MethodHook() {
 					@Override
-					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-						// set it to false
-						param.args[0] = false;
-					}				
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						// get layout params
+						WindowManager.LayoutParams layoutParams = (WindowManager.LayoutParams)param.getResult();
+						layoutParams.height = 0;
+					}
 				});
-				XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBar", lpparam.classLoader, "shouldDisableNavbarGestures", new XC_MethodReplacement() {
+				XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBar", lpparam.classLoader, "getSearchLayoutParams", ViewGroup.LayoutParams.class, new XC_MethodHook() {
 					@Override
-					protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-						// return true
-						param.setResult(true);
-						return true;
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						// get layout params
+						WindowManager.LayoutParams layoutParams = (WindowManager.LayoutParams)param.getResult();
+						layoutParams.height = 0;
+					}
+				});
+				XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBar", lpparam.classLoader, "getGlobalAccessWindowParams", new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						// get layout params
+						WindowManager.LayoutParams layoutParams = (WindowManager.LayoutParams)param.getResult();
+						layoutParams.height = 0;
 					}
 				});
 			} catch (Exception ex) {
 				// report it
-				XposedBridge.log(String.format("PieSupport - failed 'enableGlobalAccess' with error: %s", ex.getMessage()));
+				XposedBridge.log(String.format("PieSupport - failed set layout params with error: %s", ex.getMessage()));
 			}
-		}		
+		}
+	}
+
+	@Override
+	public void initZygote(StartupParam startupParam) throws Throwable {
+		XposedHelpers.findAndHookMethod("com.android.internal.policy.impl.PhoneWindowManager", null, "getNonDecorDisplayHeight", int.class, int.class, int.class, new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				// get full height
+				int fullHeight = (Integer)param.args[1];
+				
+				// set 1px less only
+				param.setResult(fullHeight - 1);
+			}			
+		});
+		XposedHelpers.findAndHookMethod("com.android.internal.policy.impl.PhoneWindowManager", null, "setInitialDisplaySize", Display.class, int.class, int.class, int.class, new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				// get array
+				int[] mNavigationBarHeightForRotation = (int[])XposedHelpers.getObjectField(param.thisObject, "mNavigationBarHeightForRotation");
+				
+				// set all to 0
+				mNavigationBarHeightForRotation[0] = 0;
+				mNavigationBarHeightForRotation[1] = 0;
+				mNavigationBarHeightForRotation[2] = 0;
+				mNavigationBarHeightForRotation[3] = 0;
+			}
+		});
 	}
 }
