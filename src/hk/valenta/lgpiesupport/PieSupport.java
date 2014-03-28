0829a1551b50e291a6e4df7fd23e6d03655d93c5
Pavel Valenta <pvalenta.hk@gmail.com>
2014-03-28 12:18:01 +0800

    Set Global Access Panel height to 0

--- src/hk/valenta/lgpiesupport/PieSupport.java
+++ src/hk/valenta/lgpiesupport/PieSupport.java
@@ -1,13 +1,15 @@
 package hk.valenta.lgpiesupport;
 
 import android.content.res.XResources;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.XC_MethodHook.MethodHookParam;
+import de.robv.android.xposed.callbacks.XC_LayoutInflated;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
@@ -17,6 +19,21 @@ public class PieSupport implements IXposedHookInitPackageResources, IXposedHookL
 	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
 		// disable navigation bar
 		XResources.setSystemWideReplacement("android", "bool", "config_showNavigationBar", false);
+		
+		if (resparam.packageName.equals("com.android.systemui")) {
+			resparam.res.hookLayout("com.android.systemui", "layout", "global_access_panel", new XC_LayoutInflated() {
+				@Override
+				public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
+					// get it as linear layout
+					LinearLayout panel = (LinearLayout)liparam.view;
+					ViewGroup.LayoutParams params = panel.getLayoutParams();
+					
+					// set 0 height for global access panel
+					XposedBridge.log("global_access_panel height set to 0");
+					params.height = 0;
+				}
+			});
+		}
 	}
 
 	@Override
